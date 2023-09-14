b19b2a1bde9ace06c0e190df531edfdea61f35a2
Taron Avagyan <contact@taronavagyan.com>
2023-09-13 17:46:19 -0700

    functionality & testing for creating requests. functionality for getting requests by binId

--- tests/requests.test.js
+++ tests/requests.test.js
@@ -29,33 +29,33 @@ describe("Postgres Bins Table Connection", () => {
     uuid = uuidv4();
 
     // Insert new bin with path "my_bin_path" and random uuid
-    const CREATE_BIN_WITH_UUID = `INSERT INTO bins (bin_path, id) VALUES ($1, $2)`;
 
-    let result = await dbQuery(CREATE_BIN_WITH_UUID, "my_bin_path", uuid);
-    return result.rowCount > 0;
+    const CREATE_BIN_WITH_UUID = `INSERT INTO bins (bin_path, id) VALUES ($1, $2)`;
+    // let result = await dbQuery(CREATE_BIN_WITH_UUID, "my_bin_path", uuid);
+    await dbQuery(CREATE_BIN_WITH_UUID, "my_bin_path", uuid);
+
+    // Insert requests into newly-created bin
+    await pgPersistence.createRequest(uuid, "abcd", "POST", "/abcd");
+    await pgPersistence.createRequest(uuid, "efgh", "POST", "/efgh");
+    await pgPersistence.createRequest(uuid, "lmno", "POST", "/lmno");
   });
 
   afterAll(async () => {
     await pool.end();
   });
 
-  test("can insert to Postgres DB", async () => {
+  test("can create request", async () => {
     try {
-      const binMade = await pgPersistence.createBin("asdasd");
-      expect(binMade).toBe(true);
+      const requestMade = await pgPersistence.createRequest(
+        uuid,
+        "xzy",
+        "POST",
+        "/xyz"
+      );
+      expect(requestMade).toBe(true);
     } catch (error) {
       console.error("Error:", error);
       throw error;
     }
-  });
-
-  test("can get all bins", async () => {
-    const returnedBins = await pgPersistence.getAllBins();
-    expect(returnedBins).not.toBe(0);
-  });
-
-  test("can delete bin", async () => {
-    const deletedBin = await pgPersistence.deleteBin(uuid);
-    expect(deletedBin).toBe(true);
   });
 });
