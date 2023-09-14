d1387bd618c123b2f21272d90d0127e323a742dd
ivanfslee <ivanfslee@gmail.com>
2023-09-13 19:24:42 -0700

    Modified app.all route

--- index.js
+++ index.js
@@ -20,16 +20,25 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/', (req, res) => {
+//Route to store all http requests to a bin
+app.all('/bin/req/:bin_path', async (req, res) => {
+  let store = res.locals.store;
   console.log(req.rawHeaders)
   console.log(req.method);
   console.log(req.url);
   console.log(req.body);
-  mongo.insertOne('test', 'requests_collection', req.body)
+  let binPath = req.params.bin_path;
+  //insert into mongo database
+  //insert req.body into mongo database
+    
+  //mongoid is string type
+  let mongoId = await mongo.insertOne('test', 'requests_collection', req.body); 
+  console.log('mongoid is :', mongoId);
+  store.createRequest(binPath, mongoId, req.method, req.url);
   res.sendStatus(200);
 });
 
-//Create new bin
+//Route to create new bin
 app.post('/createBin', (req, res) => {
   let binPath = crypto.randomBytes(10).toString('hex');
   //e.g. binPath is a string - e.g. 40a65c27d6c4a79e
@@ -39,20 +48,36 @@ app.post('/createBin', (req, res) => {
   res.redirect(`/bin/${binPath}`);
 });
 
+//Route to get all bins
 app.get('/', async (req, res) => {
-  //DB query to get all bins
   console.log(req);
-  
   let store = res.locals.store;
   let allBins = await store.getAllBins();
   res.render('homepage', {allBins})
 });
 
-app.get('/bin/:bin_path', (req, res) => {
+//Route to get all requests to a bin
+app.get('/bin/:bin_path', async (req, res) => {
+  console.log('route hit');
+  let store = res.locals.store;
   const binPath = req.params.bin_path;
   
-  //DB query to get all requests with bin id 
-  let allRequests = ['request 1', 'request 2', 'request 3'];
+  //postgres query for mongo ids that have corresponding binPath 
+  let mongoIds = await store.getMongoIdsByBinPath(binPath);
+  console.log('Mongoids is: ', mongoIds);
+  
+  //mongo query to get all requests with mongoIds
+  let mongoIdObjs = mongoIds.map((mongoId) => mongo.stringIdToMongoIdObject(mongoId));
+  console.log('mongoIdObjs: ', mongoIdObjs);
+  
+  let mongoObjs = [];
+  mongoIdObjs.forEach(mongoId => mongoObjs.push(mongo.findDbObj('test', 'requests_collection', { _id : mongoId })));
+  let allRequests = Promise.all(mongoObjs).then((values) => {
+    console.log(values);
+  })
+  // let allRequests = mongo.findDbObj('test', 'requests_collection', { _id : mongoId });
+
+  // let allRequests = ['request 1', 'request 2', 'request 3'];
 
   res.render('bin', {binPath, allRequests})
 });
